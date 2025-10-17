2528d86e6d44e9311d5d50a871ee144ebb064759
Fakhrurrizal Adhirajasa <fakhrurrizaladhirajasa@gmail.com>
2025-10-17 13:52:44 +0800

    fix model

--- models/musicModel.js
+++ models/musicModel.js
@@ -9,19 +9,19 @@ export const getMusicById = (id, callback) => {
 };
 
 export const addMusic = (data, callback) => {
-  const { title, artist, genre, release_date, duration, is_favorite } = data;
+  const { title, artist, genre, release_date, duration } = data;
   db.query(
-    "INSERT INTO musics (title, artist, genre, release_date, duration, is_favorite) VALUES (?, ?, ?, ?, ?, ?)",
-    [title, artist, genre, release_date, duration, is_favorite],
+    "INSERT INTO musics (title, artist, genre, release_date, duration) VALUES (?, ?, ?, ?, ?)",
+    [title, artist, genre, release_date, duration],
     callback
   );
 };
 
 export const updateMusic = (id, data, callback) => {
-  const { title, artist, genre, release_date, duration, is_favorite } = data;
+  const { title, artist, genre, release_date, duration } = data;
   db.query(
-    "UPDATE musics SET title=?, artist=?, genre=?, release_date=?, duration=?, is_favorite=? WHERE id=?",
-    [title, artist, genre, release_date, duration, is_favorite, id],
+    "UPDATE musics SET title=?, artist=?, genre=?, release_date=?, duration=? WHERE id=?",
+    [title, artist, genre, release_date, duration, id],
     callback
   );
 };
