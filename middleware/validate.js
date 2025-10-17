01ecbf77525eb6e2797633d2293955e0438d864a
Fakhrurrizal Adhirajasa <fakhrurrizaladhirajasa@gmail.com>
2025-10-17 13:43:01 +0800

    perbaiki error

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,7 +1,9 @@
 export default function validate(req, res, next) {
-  const { title, album, genre, release_date, duration } = req.body;
-  if (!title || !album || !genre || !release_date || !duration) {
+  const { title, artist, genre, release_date, duration } = req.body;
+
+  if (!title || !artist || !genre || !release_date || !duration) {
     return res.status(400).json({ message: "⚠️ All fields are required" });
   }
+
   next();
 }
